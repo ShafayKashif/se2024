3dba8bd0f53f3cdbe27d37d6bd5dfdf721679975
ShebuAli <98877936+ShebuAli@users.noreply.github.com>
2024-01-22 18:16:12 +0500

    Expanded boiler

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema
+
+const UsersSchema = new Schema({
+    username: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+UsersSchema.methods.comparePassword = function(candidatePassword) {
+    // compare hash of the passwords
+    return this.password === candidatePassword;
+  };
+
+
+const Users = mongoose.model('Users', UsersSchema);
+export default Users;
+

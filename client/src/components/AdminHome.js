b481139d435e341b1175fe40df8250dace3080bc
talha110-cyber <25100041@lums.edu.pk>
2024-02-10 17:03:20 +0500

    Update AdminHome.js

--- client/src/components/AdminHome.js
+++ client/src/components/AdminHome.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/AdminHome.css";
+import "../styles/CourierHome.css";
 
 const AdminHomePage = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <nav>
-        <Link to="/admin/see-vendor-reviews">See Vendor Reviews</Link>
-        <Link to="/admin/see-vendor-requests">See Vendor Requests</Link>
-        <Link to="/admin/ban-vendors">Ban Vendors</Link>
-        <Link to="/admin/see-courier-requests">See Courier Requests</Link>
+        <Link className="sub-button-Home" to="/admin/see-vendor-reviews">See Vendor Reviews</Link> <br /> 
+        <Link className="sub-button-Home" to="/admin/see-vendor-requests">See Vendor Requests</Link> <br /> 
+        <Link className="sub-button-Home" to="/admin/ban-vendors">Ban Vendors</Link><br /> 
+        <Link className="sub-button-Home" to="/admin/see-courier-requests">See Courier Requests</Link>
       </nav>
     </div>
   );
